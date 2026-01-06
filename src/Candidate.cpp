6c6702debde5001291a38b795284996e3f5c1b32
Jannis Wagner <jwa@tp4.rub.de>
2026-01-06 14:28:51 +0100

    added clip for traj.-length of secondaries to avoid negative values through faster than light diffusive Transport

--- src/Candidate.cpp
+++ src/Candidate.cpp
@@ -172,7 +172,7 @@ void Candidate::addSecondary(int id, double energy, double w, std::string tagOri
 void Candidate::addSecondary(int id, double energy, Vector3d position, double w, std::string tagOrigin) {
 	ref_ptr<Candidate> secondary = new Candidate;
 	secondary->setRedshift(redshift);
-	secondary->setTrajectoryLength(trajectoryLength - (current.getPosition() - position).getR());
+	secondary->setTrajectoryLength(clip(trajectoryLength - (current.getPosition() - position).getR()), 0, trajectorylength);
 	secondary->setTime(time - (current.getPosition() - position).getR() / getVelocity());
 	secondary->setWeight(weight * w);
 	secondary->setTagOrigin(tagOrigin);
